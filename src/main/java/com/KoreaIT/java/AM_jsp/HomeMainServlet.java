7779af234660ff9d66b6633dad98f00d4b847538
kqjm1 <kqk9269@gmail.com>
2025-04-27 23:06:42 +0900

    homr_spring_install

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/HomeMainServlet.java
@@ -0,0 +1,19 @@
+package com.KoreaIT.java.AM_jsp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home/main2")
+public class HomeMainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("hello world22222");
+	}
+
+
+}
